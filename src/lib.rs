7ed7e8f153567dc75723dfc3d8ce43dcdbe9fef4
Makoto Dejima <makotodejima@gmail.com>
2020-10-09 12:33:51 +0200

    feat: save snapshots from notion table

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,12 @@ extern crate diesel;
 mod models;
 mod schema;
 
+use chrono::NaiveDate;
+use diesel::pg::upsert::on_constraint;
 use diesel::prelude::*;
 use diesel::PgConnection;
-use models::Account;
+use models::{Account, NewSnapshot, Snapshot};
+use std::collections::HashMap;
 use std::io::{stdin, Read};
 
 pub struct DieselDemo {
@@ -24,8 +27,10 @@ impl DieselDemo {
 
     pub fn run(&self) {
         self.display_accounts();
+        self.display_snapshots();
         // TODO: subcommand to add
-        self.add_new_account();
+        // self.add_new_account();
+        // self.create_new_snapshot();
     }
 
     fn display_accounts(&self) {
@@ -34,7 +39,7 @@ impl DieselDemo {
         let all_accounts = accounts
             .load::<Account>(&self.database_connection)
             .expect("Error getting accounts");
-        println!("Displaying all accounts");
+        println!("\n *Displaying all accounts");
         println!("---");
 
         for account in all_accounts {
@@ -45,36 +50,83 @@ impl DieselDemo {
         }
     }
 
-    fn add_new_account(&self) -> Account {
+    fn display_snapshots(&self) {
+        use schema::snapshots::dsl::*;
+
+        let all_snapshots = snapshots
+            .load::<Snapshot>(&self.database_connection)
+            .expect("Error getting snapshots");
+        println!("\n *Displaying all snapshots");
+        println!("---");
+
+        for snapshot in all_snapshots {
+            println!("Id: {}", snapshot.id);
+            println!("Account: {}", snapshot.account_id);
+            println!("Date: {}", snapshot.date);
+            println!("Amount: {}", snapshot.amount);
+            println!("---");
+        }
+    }
+
+    pub fn add_new_account(
+        &self,
+        id: String,
+        name: String,
+        currency: String,
+        description: String,
+    ) -> Result<Account, diesel::result::Error> {
         use schema::accounts;
+        use schema::accounts::id as id_column;
+
         println!("Creating new account...");
 
-        println!("Id?");
-        let mut id = String::new();
-        stdin().read_line(&mut id).unwrap();
-
-        println!("Account name?");
-        let mut name = String::new();
-        stdin().read_line(&mut name).unwrap();
-
-        println!("Currency?");
-        let mut currency = String::new();
-        stdin().read_line(&mut currency).unwrap();
-
-        println!("Enter description for this account");
-        let mut description = String::new();
-        stdin().read_line(&mut description).unwrap();
-
         let new_account = Account {
-            id: id.trim().parse().unwrap(),
-            name: name.trim().parse().unwrap(),
-            currency: currency.trim().parse().unwrap(),
-            description: description.trim().parse().unwrap(),
+            id,
+            name,
+            currency,
+            description,
         };
 
         diesel::insert_into(accounts::table)
             .values(&new_account)
-            .get_result(&self.database_connection)
-            .expect("Error: failed to insert new account.")
+            .on_conflict(id_column)
+            .do_nothing()
+            .get_result::<Account>(&self.database_connection)
+    }
+
+    pub fn create_new_snapshot(&self, account_id: String, ymd_string: String, amount: i32) {
+        use schema::snapshots;
+
+        println!("Creating new snapshot...");
+
+        let new_snapshot = NewSnapshot {
+            account_id,
+            amount,
+            date: ymd_string
+                .parse::<NaiveDate>()
+                .expect("Error: Failed to parse date string"),
+        };
+
+        if let Err(err) = diesel::insert_into(snapshots::table)
+            .values(&new_snapshot)
+            .on_conflict(on_constraint("snapshot_unique"))
+            .do_nothing()
+            .get_result::<Snapshot>(&self.database_connection)
+        {
+            println!(
+                "Failed to insert snapshot. Maybe already there? Error: {}",
+                err
+            );
+        }
+    }
+
+    pub fn update_snapshots(&self, account_id: String, amounts_by_date: HashMap<String, i32>) {
+        println!(
+            "Updating snapshots for {} with notion's table data...",
+            &account_id
+        );
+        for (date, amount) in amounts_by_date {
+            self.create_new_snapshot(account_id.to_owned(), date, amount);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,79 @@ extern crate serde_json;
 
 use diesel_demo::DieselDemo;
 use dotenv::dotenv;
-use reqwest::Error;
 use serde_json::value::Value;
+use std::collections::HashMap;
+use std::convert::TryFrom;
 use std::env;
 
 fn main() -> Result<(), reqwest::Error> {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("Error loading database url");
     let demo_db = DieselDemo::new(database_url);
+    let notion_api_url = env::var("NOTION_API_URL").expect("Error loading notion api url");
+    let res = reqwest::blocking::get(&notion_api_url)?.json::<Value>()?;
+
+    let notion_table: Vec<Value>;
+    if let Value::Array(vec) = res {
+        notion_table = vec;
+    } else {
+        panic!("Error: Received non vector response");
+    };
+
+    for notion_row in notion_table {
+        if let Value::Object(row) = notion_row {
+            let mut account_id = String::new();
+            let mut account_type = String::new();
+            let mut account_name = String::new();
+            let mut account_currency = String::new();
+            let mut amounts_by_date = HashMap::new();
+            for (key, val) in row {
+                match key.as_str() {
+                    "id" => {
+                        account_id = val.as_str().unwrap().to_string();
+                    }
+                    "Type" => {
+                        account_type = val.as_str().unwrap().to_string();
+                    }
+                    "Name" => {
+                        account_name = val.as_str().unwrap().to_string();
+                    }
+                    "Currency" => {
+                        account_currency = val.as_str().unwrap().to_string();
+                    }
+                    _ => {
+                        if let Value::Number(num) = val {
+                            let num_as_i64 = num.as_i64().unwrap();
+                            let amount = i32::try_from(num_as_i64)
+                                .expect("Error: Failed to convert from i64");
+                            amounts_by_date.insert(key, amount);
+                        } else {
+                            println!("Error: Unknown type of key found");
+                        }
+                    }
+                }
+            }
+
+            let result = demo_db.add_new_account(
+                account_id.to_owned(),
+                account_name.to_owned(),
+                account_currency,
+                account_type,
+            );
+
+            match result {
+                Ok(new_account) => println!("Added new accounts: {}", new_account.name),
+                Err(err) => println!(
+                    "Failed to insert '{}'. Maybe already there? Error: {}",
+                    account_name, err
+                ),
+            }
+
+            demo_db.update_snapshots(account_id.to_owned(), amounts_by_date)
+        } else {
+            println!("Failed to find rows.");
+        }
+    }
     demo_db.run();
-    // let notion_api_url = env::var("NOTION_API_URL").expect("Error loading notion api url");
-    // let res = reqwest::blocking::get(&notion_api_url)?.json::<Value>()?;
-    // println!("{:#?}", res);
     Ok(())
 }
-
-// CREATE TABLE snapshots (
-//   id SERIAL PRIMARY KEY,
-//   account_id FOREIGN KEY (accounts id),
-//   date DATE,
-//   amount Int
-// )
