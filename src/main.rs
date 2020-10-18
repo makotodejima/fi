04c1653de5589834382cd4426459196bf3799ba0
Makoto Dejima <makotodejima@gmail.com>
2020-10-18 10:07:32 +0200

    feat: sync all data

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use diesel::prelude::*;
 use dotenv::dotenv;
-use fi::account::sync;
+use fi::account::Account;
 use fi::cli::Cli;
 use fi::currency::Currency;
 use fi::{delete_data, display_history, display_latest_sum, display_net_worth};
@@ -23,10 +23,13 @@ fn run(database_url: &str, command: Cli) -> Result<(), Box<dyn Error>> {
     let conn = PgConnection::establish(&database_url).expect("Error connecting to the database");
     match command {
         Cli::Pull { currency } => {
+            if currency == "all".to_string() {
+                sync_all(&conn).expect("Error occurred while synching");
+            }
             let given_currency = Currency::from_str(&currency);
             let notion_api_url = get_notion_api_url(&given_currency);
             let res = reqwest::blocking::get(&notion_api_url)?.json::<Value>()?;
-            sync(&conn, res);
+            Account::sync(&conn, res);
         }
         Cli::History { currency } => {
             let given_currency = Currency::from_str(&currency);
@@ -47,13 +50,25 @@ fn run(database_url: &str, command: Cli) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn get_notion_api_url(currency: &Currency) -> String {
+fn get_notion_api_url(currency: &Currency) -> String {
     let mut notion_api_url = String::from("NOTION_API_URL_");
     notion_api_url.push_str(currency.as_str());
     match env::var(notion_api_url) {
         Ok(url) => url,
         Err(err) => panic!("Failed to get notion api url. Error: {}", err),
     }
+}
+
+fn sync_all(conn: &PgConnection) -> Result<(), Box<dyn Error>> {
+    use std::process::exit;
+    let currencies = [Currency::EUR, Currency::JPY, Currency::USD];
+    for cur in &currencies {
+        let notion_api_url = get_notion_api_url(cur);
+        let res = reqwest::blocking::get(&notion_api_url)?.json::<Value>()?;
+        Account::sync(&conn, res);
+    }
+    println!("Synching all accounts and snapshots completed.");
+    exit(0);
 }
 
 fn handle_error<T>(res: Result<T, Box<dyn Error>>) -> T {
